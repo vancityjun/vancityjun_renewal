be45a3e7b94e9f3291f9aedddf3c59ffdd29e7f9
vancityjun <vancityjun@github.com>
2019-12-30 10:23:21 -0800

    change to functional component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import "./App.scss";
 import { TweenMax, ScrollToPlugin, Power1, Linear, Draggable } from "gsap/all";
 import $ from "jquery";
@@ -13,20 +13,16 @@ import Slide from "./component/Slide.js";
 import { projects } from "./data.json";
 import Cursor from "./component/Cursor.js";
 import IsMobile from "./helper/IsMobile";
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.slidehandler = this.slidehandler.bind(this);
-  }
 
-  componentDidMount() {
+const App = () => {
+  useEffect(() => {
+    console.log(IsMobile());
     const swiper = new Swiper(".blog-slider", {
       spaceBetween: 30,
       effect: "fade", //fade slide
       loop: true,
       preventClicks: false,
       preventClicksPropagation: false,
-      // hashNavigation:true,
       mousewheel: {
         sensitivity: 3
       },
@@ -52,166 +48,165 @@ class App extends Component {
       menuTransform();
     });
 
-    $(function() {
-      // $('.top_bar').midnight();
-      const contentOpen = () => {
+    // $('.top_bar').midnight();
+    const contentOpen = () => {
+      $(".swiper-slide-active")
+        .find(".content_wrapper")
+        .css("right", "-45%");
+      var htmlWidth = window.innerWidth;
+      if (htmlWidth <= 576) {
         $(".swiper-slide-active")
           .find(".content_wrapper")
-          .css("right", "-45%");
-        var htmlWidth = window.innerWidth;
-        if (htmlWidth <= 576) {
-          $(".swiper-slide-active")
-            .find(".content_wrapper")
-            .css("right", "-100%");
-        }
+          .css("right", "-100%");
+      }
+      $(".swiper-slide-active")
+        .find(".blog-slider__img")
+        .css("right", "0px");
+      $(".swiper-slide")
+        .find(".videoWrapper")
+        .css("display", "block");
+      $(".closeBtn").fadeIn(600);
+      $(".swiper-slide-active")
+        .find(".project-info")
+        .addClass("active");
+      if (htmlWidth > 576) {
         $(".swiper-slide-active")
-          .find(".blog-slider__img")
-          .css("right", "0px");
-        $(".swiper-slide")
-          .find(".videoWrapper")
-          .css("display", "block");
-        $(".closeBtn").fadeIn(600);
-        $(".swiper-slide-active")
-          .find(".project-info")
-          .addClass("active");
-        if (htmlWidth > 576) {
-          $(".swiper-slide-active")
-            .find(".scrollDown")
-            .fadeIn(600);
-        }
-        $(".swiper-slide-active")
-          .find(".project-content")
-          .addClass("active");
-        $(".swiper-slide-active")
-          .find(".info-background")
-          .fadeIn("600");
-        $(".blog-slider__pagination").fadeOut(400);
-        $(".arrows").fadeOut(400);
-        $(".swiper-wrapper").addClass("swiper-no-swiping");
-        swiper.mousewheel.disable();
-        swiper.keyboard.disable();
-        $(".swiper-slide-active").addClass("scroll");
-        $(".scroll").animate(
-          {
-            scrollTop: 0
-          },
-          0
-        );
-      };
-      /* VIEW CASE BUTTON */
-      $(".blog-slider__button").on("click", function(e) {
-        e.preventDefault();
-        contentOpen();
-      });
+          .find(".scrollDown")
+          .fadeIn(600);
+      }
+      $(".swiper-slide-active")
+        .find(".project-content")
+        .addClass("active");
+      $(".swiper-slide-active")
+        .find(".info-background")
+        .fadeIn("600");
+      $(".blog-slider__pagination").fadeOut(400);
+      $(".arrows").fadeOut(400);
+      $(".swiper-wrapper").addClass("swiper-no-swiping");
+      swiper.mousewheel.disable();
+      swiper.keyboard.disable();
+      $(".swiper-slide-active").addClass("scroll");
+      $(".scroll").animate(
+        {
+          scrollTop: 0
+        },
+        0
+      );
+    };
+    /* VIEW CASE BUTTON */
+    $(".blog-slider__button").on("click", function(e) {
+      e.preventDefault();
+      contentOpen();
+    });
 
+    $(".swiper-slide")
+      .find(".info-background")
+      .css("display", "none");
+
+    const contentClose = fod => {
+      $(".scroll").animate(
+        {
+          scrollTop: 0
+        },
+        400
+      );
+      $(".blog-slider__img").css({
+        transform: "translate3d(0, 0, 0) scale(1)"
+      });
+      $(".swiper-slide").removeClass("scroll");
+      $(".swiper-slide")
+        .find(".content_wrapper")
+        .css("right", "0px");
+      $(".swiper-slide")
+        .find(".videoWrapper")
+        .css("display", "none");
+      $(".swiper-slide")
+        .find(".blog-slider__img")
+        .css("right", "300px");
+      $(".closeBtn").fadeOut(600);
+      $(".swiper-slide")
+        .find(".project-info")
+        .removeClass("active");
+      $(".swiper-slide")
+        .find(".scrollDown")
+        .fadeOut(fod);
+      $(".swiper-slide")
+        .find(".project-content")
+        .removeClass("active");
       $(".swiper-slide")
         .find(".info-background")
-        .css("display", "none");
-
-      const contentClose = fod => {
-        $(".scroll").animate(
-          {
-            scrollTop: 0
-          },
-          400
-        );
-        $(".blog-slider__img").css({
-          transform: "translate3d(0, 0, 0) scale(1)"
-        });
-        $(".swiper-slide").removeClass("scroll");
-        $(".swiper-slide")
-          .find(".content_wrapper")
-          .css("right", "0px");
-        $(".swiper-slide")
-          .find(".videoWrapper")
-          .css("display", "none");
-        $(".swiper-slide")
-          .find(".blog-slider__img")
-          .css("right", "300px");
-        $(".closeBtn").fadeOut(600);
-        $(".swiper-slide")
-          .find(".project-info")
-          .removeClass("active");
-        $(".swiper-slide")
-          .find(".scrollDown")
-          .fadeOut(fod);
-        $(".swiper-slide")
-          .find(".project-content")
-          .removeClass("active");
-        $(".swiper-slide")
-          .find(".info-background")
-          .fadeOut(fod);
-        $(".blog-slider__pagination").fadeIn(fod);
-        $(".arrows").fadeIn(fod);
-        $(".swiper-wrapper").removeClass("swiper-no-swiping");
-        swiper.mousewheel.enable();
-        swiper.keyboard.enable();
-      };
-      /* CLOSE BUTTON */
-      $(".closeBtn").on("click", function(e) {
-        e.preventDefault();
-        contentClose(400);
-      });
-
-      $(".menu a").click(function(e) {
-        e.preventDefault();
-        $(".toggle-menu").removeClass("active");
-        $(".menu").fadeOut(600);
-        contentClose(400);
-      });
-      $(".scrollTop").on("click", function(e) {
-        e.preventDefault();
-        $(".scroll").animate(
-          {
-            scrollTop: 0
-          },
-          400
-        );
-      });
-      // $(".scrollDown").on("click", function(e) {
-      //   e.preventDefault();
-      //   $(".scroll").animate(
-      //     {
-      //       scrollTop: $(".project-header").height()
-      //     },
-      //     500,
-      //     "linear"
-      //   );
-      // });
-
-      const wrap = $(".blog-slider__img");
-      wrap.on("mousewheel", function() {
-        // customScroll();
-      });
-      const customScroll = event => {
-        let delta = 0;
-        if (!event) {
-          event = window.event;
-        }
-        if (event.wheelDelta) {
-          delta = event.wheelDelta / 120;
-        } else if (event.detail) {
-          delta = -event.detail / 3;
-        }
-        if (delta) {
-          const scrollTop = $(".scroll").scrollTop();
-          const finScroll = scrollTop - parseInt(delta * 100) * 3;
-          console.log(finScroll);
-          TweenMax.to($(".scroll"), 1, {
-            scrollTo: { y: finScroll },
-            ease: Power1.easeOut,
-            overwrite: 5
-          });
-        }
-        if (event.preventDefault) {
-          event.preventDefault();
-        }
-        event.returnValue = false;
-      };
-      if (window.addEventListener) {
-        window.addEventListener("DOMMouseScroll", customScroll, false);
-      }
+        .fadeOut(fod);
+      $(".blog-slider__pagination").fadeIn(fod);
+      $(".arrows").fadeIn(fod);
+      $(".swiper-wrapper").removeClass("swiper-no-swiping");
+      swiper.mousewheel.enable();
+      swiper.keyboard.enable();
+    };
+    /* CLOSE BUTTON */
+    $(".closeBtn").on("click", function(e) {
+      e.preventDefault();
+      contentClose(400);
     });
+
+    $(".menu a").click(function(e) {
+      e.preventDefault();
+      $(".toggle-menu").removeClass("active");
+      $(".menu").fadeOut(600);
+      contentClose(400);
+    });
+    $(".scrollTop").on("click", function(e) {
+      e.preventDefault();
+      $(".scroll").animate(
+        {
+          scrollTop: 0
+        },
+        400
+      );
+    });
+    // $(".scrollDown").on("click", function(e) {
+    //   e.preventDefault();
+    //   $(".scroll").animate(
+    //     {
+    //       scrollTop: $(".project-header").height()
+    //     },
+    //     500,
+    //     "linear"
+    //   );
+    // });
+
+    const wrap = $(".blog-slider__img");
+    wrap.on("mousewheel", function() {
+      // customScroll();
+    });
+    const customScroll = event => {
+      let delta = 0;
+      if (!event) {
+        event = window.event;
+      }
+      if (event.wheelDelta) {
+        delta = event.wheelDelta / 120;
+      } else if (event.detail) {
+        delta = -event.detail / 3;
+      }
+      if (delta) {
+        const scrollTop = $(".scroll").scrollTop();
+        const finScroll = scrollTop - parseInt(delta * 100) * 3;
+        console.log(finScroll);
+        TweenMax.to($(".scroll"), 1, {
+          scrollTo: { y: finScroll },
+          ease: Power1.easeOut,
+          overwrite: 5
+        });
+      }
+      if (event.preventDefault) {
+        event.preventDefault();
+      }
+      event.returnValue = false;
+    };
+    if (window.addEventListener) {
+      window.addEventListener("DOMMouseScroll", customScroll, false);
+    }
+
     //menu onclick
     $(".link").on("click", function(e) {
       e.preventDefault();
@@ -224,11 +219,10 @@ class App extends Component {
         wrapHeight = menu.height(),
         scrollWrap = $(".scrollWrap"),
         listHeight = scrollWrap.height();
-      !IsMobile
+      IsMobile()
         ? Draggable.create(".scrollWrap", { type: "y", throwProps: true })
         : menu.on("mousemove", function(e) {
             const dP = e.pageY / wrapHeight;
-
             TweenMax.to(scrollWrap, 0.1, {
               y: -(listHeight * dP - listHeight / 2),
               ease: Linear.easeNone
@@ -322,55 +316,48 @@ class App extends Component {
     $(document).ready(function() {
       menuActive();
     });
-  }
-
-  slidehandler(i) {
+  });
+  const slidehandler = i => {
     this.swiper.slideTo(i);
-  }
-  render() {
-    const slides = projects.map((projects, i) => {
-      return (
-        <Slide
-          key={i}
-          id={i}
-          title={projects.title}
-          category={projects.category}
-          background={projects.background}
-          date={projects.date}
-          description={projects.description}
-          url={projects.url}
-          pc={projects.pc}
-          mobile={projects.mobile}
-          customContent={projects.customContent}
-        />
-      );
-    });
+  };
+  const slides = projects.map((projects, i) => {
     return (
-      <div className="App">
-        <div className="wrapper">
-          <Topbar />
-          <Menu projects={projects} />
-          <div className="blog-slider">
-            <div className="blog-slider__wrp swiper-wrapper">
-              <Profile />
-              {slides}
-            </div>
-            <div className="arrows">
-              <div className="navigation blog-button-prev hover-target">
-                prev
-              </div>
-              <span className="space"></span>
-              <div className="navigation blog-button-next hover-target">
-                next
-              </div>
-            </div>
-            <div className="blog-slider__pagination"></div>
-          </div>
-        </div>
-        {IsMobile ? <Cursor /> : null}
-      </div>
+      <Slide
+        key={i}
+        id={i}
+        title={projects.title}
+        category={projects.category}
+        background={projects.background}
+        date={projects.date}
+        description={projects.description}
+        url={projects.url}
+        pc={projects.pc}
+        mobile={projects.mobile}
+        customContent={projects.customContent}
+      />
     );
-  }
-}
+  });
+  return (
+    <div className="App">
+      <div className="wrapper">
+        <Topbar />
+        <Menu projects={projects} />
+        <div className="blog-slider">
+          <div className="blog-slider__wrp swiper-wrapper">
+            <Profile />
+            {slides}
+          </div>
+          <div className="arrows">
+            <div className="navigation blog-button-prev hover-target">prev</div>
+            <span className="space"></span>
+            <div className="navigation blog-button-next hover-target">next</div>
+          </div>
+          <div className="blog-slider__pagination"></div>
+        </div>
+      </div>
+      {IsMobile() ? null : <Cursor />}
+    </div>
+  );
+};
 
 export default App;
